29215e3a89939ff7e0ad07c86c49145e9f7d60d9
Javier Villanueva <javier@medialounge.co.uk>
2022-07-03 22:07:51 +0100

    Add vote functionality

--- contracts/Governor.sol
+++ contracts/Governor.sol
@@ -9,8 +9,91 @@ contract Governor is Ownable {
     string email;
   }
 
+  struct Project {
+    string name;
+    string description;
+    uint256 date;
+    uint256 votes;
+    bool approved;
+  }
+
+  uint256 public quorum = 80;
   uint256 public memberCount;
+  uint256 public projectCount;
+
+  uint256 public constant VOTING_PERIOD = 1 weeks;
+
   mapping(address => Member) private members;
+  mapping(uint256 => Project) private projects;
+  mapping(uint256 => address[]) private votes;
+
+  modifier onlyMembers(address _wallet) {
+    require(
+      keccak256(abi.encodePacked(members[_wallet].name)) !=
+        keccak256(abi.encodePacked("")),
+      "Not a member"
+    );
+    _;
+  }
+
+  function propose(string calldata _name, string calldata _description)
+    external
+    onlyMembers(msg.sender)
+  {
+    projects[projectCount] = Project(
+      _name,
+      _description,
+      block.timestamp,
+      0,
+      false
+    );
+    projectCount++;
+  }
+
+  function vote(uint256 _projectId) external onlyMembers(msg.sender) {
+    require(
+      keccak256(abi.encodePacked(projects[_projectId].name)) !=
+        keccak256(abi.encodePacked("")),
+      "Project does not exist"
+    );
+    require(projects[_projectId].approved == false, "Project already approved");
+    require(
+      projects[_projectId].date + VOTING_PERIOD > block.timestamp,
+      "Vote period has ended"
+    );
+
+    for (uint256 i = 0; i < projects[_projectId].votes; i++) {
+      if (votes[_projectId][i] == msg.sender) {
+        revert("Already voted");
+      }
+    }
+
+    projects[_projectId].votes += 1;
+    votes[_projectId].push(msg.sender);
+
+    if ((projects[_projectId].votes / memberCount) * 100 >= quorum) {
+      projects[_projectId].approved = true;
+    }
+  }
+
+  function getProjects() external view returns (Project[] memory) {
+    Project[] memory result = new Project[](projectCount);
+
+    for (uint256 i = 0; i < projectCount; i++) {
+      Project storage project = projects[i];
+      result[i] = project;
+    }
+
+    return result;
+  }
+
+  function getProject(uint256 _projectId) public view returns (Project memory) {
+    return projects[_projectId];
+  }
+
+  function getMember(address _wallet) public view returns (Member memory) {
+    return members[_wallet];
+  }
 
   function register(
     string calldata _name,
@@ -23,26 +106,16 @@ contract Governor is Ownable {
       "Member already registered"
     );
 
-    Member memory member;
-    member.name = _name;
-    member.email = _email;
-
-    members[_wallet] = member;
+    members[_wallet] = Member(_name, _email);
     memberCount++;
   }
 
-  function remove(address _wallet) external onlyOwner {
-    require(
-      keccak256(abi.encodePacked(members[_wallet].name)) !=
-        keccak256(abi.encodePacked("")),
-      "Member not registered"
-    );
-
+  function remove(address _wallet) external onlyOwner onlyMembers(_wallet) {
     delete members[_wallet];
     memberCount--;
   }
 
-  function getMember(address _wallet) public view returns (Member memory) {
-    return members[_wallet];
+  function setQuorum(uint256 _quorum) external onlyOwner {
+    quorum = _quorum;
   }
 }
--- test/Governor.spec.js
+++ test/Governor.spec.js
@@ -26,7 +26,7 @@ describe('Governor', function () {
 
   it('Deletes existing member', async () => {
     await expect(governor.remove(addrs[0].address)).to.be.revertedWith(
-      'Member not registered'
+      'Not a member'
     );
 
     await governor.register('Test', 'test@email.com', addrs[0].address);
@@ -34,5 +34,38 @@ describe('Governor', function () {
     await governor.remove(addrs[0].address);
 
     expect(await governor.memberCount()).to.equal(0);
+  });
+
+  it('Allows members to propose projects', async () => {
+    await expect(
+      governor.propose('Test Name', 'Test Description')
+    ).to.be.revertedWith('Not a member');
+
+    await governor.register('Test', 'test@email.com', addrs[0].address);
+
+    await governor.connect(addrs[0]).propose('Test Name', 'Test Description');
+
+    expect((await governor.getProjects()).length).to.equal(1);
+  });
+
+  it('Allows members to vote on projects', async () => {
+    await governor.register('Test', 'test@email.com', addrs[0].address);
+
+    await expect(governor.connect(addrs[0]).vote(0)).to.be.revertedWith(
+      'Project does not exist'
+    );
+
+    await governor.connect(addrs[0]).propose('Test Name', 'Test Description');
+
+    await governor.connect(addrs[0]).vote(0);
+
+    const project = await governor.getProject(0);
+
+    expect(project.votes).to.equal(1);
+    expect(project.approved).to.equal(true);
+
+    await expect(governor.connect(addrs[0]).vote(0)).to.be.revertedWith(
+      'Project already approved'
+    );
   });
 });
